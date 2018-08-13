07c2a6a0f165fc49c795279c039475e1da34f96e
LakshmiManasaNuthalapati <41769901+LakshmiManasaNuthalapati@users.noreply.github.com>
2018-08-13 11:30:15 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,8 @@
+def main():
+       ch=input("enter any character:")
+       if((ch>='a' and ch<'z') or (ch>='a' and ch<='z')):
+                 print("alphabet")
+   else:
+              print("not an alphabet")
+if__name__=='__main__':
+    main()
