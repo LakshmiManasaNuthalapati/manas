82d66216c5f7d1cd01d536a83b0e868ac315e6dd
LakshmiManasaNuthalapati <41769901+LakshmiManasaNuthalapati@users.noreply.github.com>
2018-08-13 12:33:11 +0530

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,11 @@
+def main():
+num = int(input(""))
+factorial = 1
+if num == 0:
+print("the factorial of 0 is 1")
+elif num>0
+for i i range(1,num+1):
+    factorial=factorial*i
+    print("factorial")
+if__name__ == '__main__'  :
+     main()
