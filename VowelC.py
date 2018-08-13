33659914cd77bbe009ddefb50daf5abca275ed9d
LakshmiManasaNuthalapati <41769901+LakshmiManasaNuthalapati@users.noreply.github.com>
2018-08-13 11:21:00 +0530

    Create VowelC.py

---
+++ VowelC.py
@@ -0,0 +1,8 @@
+def main():
+l = input("input a letter of the alphabet:")
+if l in ('a','e','i','o','u'):
+        print("%s is a vowel."% 1)
+else:
+        print("%s is a consonat."% 1)
+if __name__=='__main__':    
+        main()
